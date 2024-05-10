d32ff4a823559afd0881636dca830e4c0dfa8e16
3shisan3 <shisan233@gmail.com>
2024-05-10 15:24:57 +0800

    增加python中的示例文件
    Signed-off-by: 3shisan3 <shisan233@gmail.com>

---
+++ python_moudle/read_rosbag.py
@@ -0,0 +1,15 @@
+import rosbag
+
+# 获取ros包中指定topic的内容，二进制数据返回
+def getRosMsgInfo(bag_path, topic_name):
+    bag = rosbag.Bag(bag_path)
+    index = 0
+    for topic, msg, t in bag.read_messages(topics=topic_name):
+        header = msg.header
+        header_seq = header.seq 
+        stamp_sec = header.stamp.secs
+        stamp_nsec = header.stamp.nsecs
+        data = msg.data #bytes
+        print('{:0>5d} {} {} {}'.format(index, header_seq, stamp_sec, stamp_nsec))
+        index += 1
+        
