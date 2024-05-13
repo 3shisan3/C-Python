ece3ca271cf6b227abe09a06064aeb5b5c3e2d7b
3shisan3 <shisan233@gmail.com>
2024-05-13 12:08:34 +0800

    调整代码结构
    Signed-off-by: 3shisan3 <shisan233@gmail.com>

---
+++ c++/src/common/msg_deal.h
@@ -0,0 +1,14 @@
+/***************************************************************
+Copyright (c) 2022-2030, shisan233@sszc.live.
+All rights reserved.
+File:        msg_deal.h
+Version:     1.0
+Author:      cjx
+start date: 2024-5-13
+Description: ros消息解析的父类
+Version history
+
+[序号]    |   [修改日期]  |   [修改者]   |   [修改内容]
+1             2024-5-13      cjx        create
+
+*****************************************************************/
